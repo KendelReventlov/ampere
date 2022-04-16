308b02dd029caa5077f0f078b1b6268b95c38b81
atcomlabs <70113286+atcomlabs@users.noreply.github.com>
2022-04-16 12:20:36 -0500

    hecho

--- nucleo_optico.py
+++ nucleo_optico.py
@@ -1,12 +1,18 @@
 import cv2
-import numpy as np
+import socket
 
 objeto_video = cv2.VideoCapture(0)
 objeto_video.set(cv2.CAP_PROP_FRAME_WIDTH,720)
 objeto_video.set(cv2.CAP_PROP_FRAME_HEIGHT,480)
 exito = 1
 
-while exito:
-    exito,imagen = objeto_video.read()
-    print(np.array(imagen).shape)
-    print()
+
+exito,imagen = objeto_video.read()
+bytes = cv2.imencode(".png",imagen)[1].tobytes()
+
+conexion = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+conexion.connect(("127.0.0.1",3000))
+print(len(bytes))
+conexion.send(bytes)
+conexion.close()
+    
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,29 @@
-use std::net::{TcpListener,TcpStream};
+extern crate image;
+extern crate websocket;
+extern crate viuer;
+use std::net::{TcpListener};
 use std::io::prelude::*;
-
-fn conectar(stream: &mut TcpStream){
-
-    let mut buffer = Vec::new();
-    stream.read_to_end(&mut buffer).unwrap();
-    println!("MENSAJE RECIVIDO: {:?}",String::from_utf8(buffer).unwrap());
-}
+use viuer::{Config,print};
 
 fn main() {
     let servidor = TcpListener::bind("127.0.0.1:3000").unwrap();
+    std::process::Command::new("python").arg("nucleo_optico.py");
     for stream in servidor.incoming(){
-        conectar(&mut stream.unwrap());
+        std::thread::spawn(move ||{
+            let mut stream = stream.unwrap();
+            let mut buffer = Vec::new();
+            stream.read_to_end(&mut buffer).unwrap();
+
+            let config = Config{
+                width: Some(70),
+                height: Some(50),
+                x:0,
+                y:0,
+                ..Default::default()
+            };
+
+            let metadata = image::load_from_memory(&mut buffer).unwrap();
+            print(&metadata, &config).unwrap();
+        });
     }
 }
