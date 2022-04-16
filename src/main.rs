90f49f0a8ea7e3c7e257459171e3fdf5f0dfc3c1
atcomlabs <70113286+atcomlabs@users.noreply.github.com>
2022-04-16 12:28:40 -0500

    a

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 extern crate image;
 extern crate websocket;
-
+extern crate viuer;
 use std::net::{TcpListener};
 use std::io::prelude::*;
-
+use viuer::{Config,print};
 
 fn main() {
     let servidor = TcpListener::bind("127.0.0.1:3000").unwrap();
@@ -17,7 +17,16 @@ fn main() {
             let mut buffer = Vec::new();
             stream.read_to_end(&mut buffer).unwrap();
 
+            let config = Config{
+                width: Some(100),
+                height: Some(30),
+                x:0,
+                y:0,
+                ..Default::default()
+            };
+
             let metadata = image::load_from_memory(&mut buffer).unwrap();
+            print(&metadata, &config).unwrap();
         });
     }
 }
