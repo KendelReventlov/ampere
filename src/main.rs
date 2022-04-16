d603bb82d7b3d0a841a93e997317a961c5ff2769
atcomlabs <70113286+atcomlabs@users.noreply.github.com>
2022-04-15 20:48:15 -0500

    tercero

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 use std::net::{TcpListener,TcpStream};
 use std::io::prelude::*;
 
+fn conectar(stream: &mut TcpStream){
+
+    let mut buffer = Vec::new();
+    stream.read_to_end(&mut buffer).unwrap();
+    println!("MENSAJE RECIVIDO: {:?}",String::from_utf8(buffer).unwrap());
+}
+
 fn main() {
-    println!("Hello, world!");
+    let servidor = TcpListener::bind("127.0.0.1:3000").unwrap();
+    for stream in servidor.incoming(){
+        conectar(&mut stream.unwrap());
+    }
 }
