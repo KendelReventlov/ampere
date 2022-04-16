4c35e90cc0a0502a82cbebcb891e4e56db43c06a
atcomlabs <70113286+atcomlabs@users.noreply.github.com>
2022-04-15 21:15:47 -0500

    final

--- nucleo_optico.py
+++ nucleo_optico.py
@@ -2,6 +2,8 @@ import cv2
 import numpy as np
 
 objeto_video = cv2.VideoCapture(0)
+objeto_video.set(cv2.CAP_PROP_FRAME_WIDTH,720)
+objeto_video.set(cv2.CAP_PROP_FRAME_HEIGHT,480)
 exito = 1
 
 while exito:
