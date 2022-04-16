143a049e5a46a641467c1b688272668dc6df64d3
atcomlabs <70113286+atcomlabs@users.noreply.github.com>
2022-04-15 21:11:13 -0500

    septimo

--- nucleo_optico.py
+++ nucleo_optico.py
@@ -1,9 +1,10 @@
 import cv2
+import numpy as np
 
 objeto_video = cv2.VideoCapture(0)
 exito = 1
 
 while exito:
     exito,imagen = objeto_video.read()
-    print(len(imagen),len(imagen[0]))
+    print(np.array(imagen).shape)
     print()
