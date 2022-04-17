524a9a0d4f79b00471bf65c62f7ca51e2a338f2e
atcomlabs <70113286+atcomlabs@users.noreply.github.com>
2022-04-16 20:49:32 -0500

    make

--- nucleo_optico.py
+++ nucleo_optico.py
@@ -6,13 +6,11 @@ objeto_video.set(cv2.CAP_PROP_FRAME_WIDTH,720)
 objeto_video.set(cv2.CAP_PROP_FRAME_HEIGHT,480)
 exito = 1
 
-conexion = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-conexion.connect(("127.0.0.1",3000))
 while exito:
-
+    conexion = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    conexion.connect(("127.0.0.1",3000))
     exito,imagen = objeto_video.read()
     bytes = cv2.imencode(".png",imagen)[1].tobytes()
     conexion.send(bytes)
-    
-conexion.close()
+    conexion.close()
     
